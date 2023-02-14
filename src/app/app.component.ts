f792a9be3dd432c7b6520c52ab100a8ca88e6a98
luaroncrew <ma-kir.ru@yandex.ru>
2023-02-14 10:53:48 +0100

    fucking dependencies managed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
-import {IExec} from "iexec";
+import {IExec, IExecConfig} from "iexec";
+
 
 const VALIDATOR_APP_ADDRESS = '0xFF1204Ce914006C7479b0E362A4AC6917F84D8b3';
 const APP_CALL_PARAMETERS = 'setup'
@@ -9,13 +10,51 @@ const APP_CALL_PARAMETERS = 'setup'
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent{
-  title: string = 'hellp';
+export class AppComponent {
 
-  initWallet() {
+  ethProvider: any;
+  iexec: any;
+
+  async initWallet() {
     if (typeof window.ethereum !== 'undefined') {
       console.log('MetaMask is installed!');
     }
+    if (window.ethereum) {
+      console.log('requesting metamask...')
+        console.log("using default provider");
+        this.ethProvider = window.ethereum;
+      this.ethProvider.on("chainChanged", (_chainId: any) => window.location.reload());
+      this.ethProvider.on("accountsChanged", (_accounts: any) =>
+          window.location.reload()
+        );
+        await window.ethereum.request({
+          method: "eth_requestAccounts"
+        });
+        window.ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: "0x86",
+              chainName: "iExec Sidechain",
+              nativeCurrency: {
+                name: "xRLC",
+                symbol: "xRLC",
+                decimals: 18
+              },
+              rpcUrls: ["https://bellecour.iex.ec"],
+              blockExplorerUrls: ["https://blockscout-bellecour.iex.ec"]
+            }
+          ]
+        });
+      }
+  }
+  verifyProvider() {
+    console.log('ethereum provider: ', this.ethProvider);
+  }
+
+  initIexecUsingConfig() {
+    this.iexec = new IExecConfig({ethProvider: this.ethProvider});
+    console.log('iexec initialized: ', this.iexec)
   }
 }
 
