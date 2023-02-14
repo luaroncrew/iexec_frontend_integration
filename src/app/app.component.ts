ce70d34c15da6aa4b116e71d6cc7172995d52f46
luaroncrew <ma-kir.ru@yandex.ru>
2023-02-14 20:21:29 +0100

    add buyComputation function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from '@angular/core';
-import {IExec, IExecConfig} from "iexec";
+import {IExec, IExecConfig, IExecOrderbookModule, IExecOrderModule, IExecWalletModule} from "iexec";
 
 
 const VALIDATOR_APP_ADDRESS = '0xFF1204Ce914006C7479b0E362A4AC6917F84D8b3';
-const APP_CALL_PARAMETERS = 'setup'
+const APP_CALL_PARAMETERS = 'setup';
+const APP_CATEGORY = 0;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -55,6 +57,60 @@ export class AppComponent {
   initIexecUsingConfig() {
     this.iexec = new IExecConfig({ethProvider: this.ethProvider});
     console.log('iexec initialized: ', this.iexec)
+  }
+
+  async validateTotpCode() {
+    // fetch signed app orders for our deployed TOTP application
+    const orderBook = IExecOrderbookModule.fromConfig(this.iexec)
+    const appOrders = await orderBook.fetchAppOrderbook(
+      VALIDATOR_APP_ADDRESS
+    );
+    console.log('app orders for totp validator: ', appOrders);
+
+    // get the last order's hash
+    let firstAppOrder = appOrders.orders?.[0]
+    let firstAppOrderHash: string = firstAppOrder?.orderHash;
+    console.log('app order hash to execute: ', firstAppOrderHash);
+
+
+    // get workerPoolOrders to get the execution price
+    const workerPoolOrders = await orderBook.fetchWorkerpoolOrderbook({
+      category: APP_CATEGORY,
+      workerpool: "v7-prod.main.pools.iexec.eth" // use iExec official workerpool
+    });
+    const workerPoolOrder = workerPoolOrders.orders?.[0]
+    console.log('workerPoolOrder: ', workerPoolOrder);
+
+    // get user address
+    let wallet = IExecWalletModule.fromConfig(this.iexec);
+    const userAddress = await wallet.getAddress();
+    console.log('user address: ', userAddress);
+
+    // create RequestOrder
+    let orderModule = IExecOrderModule.fromConfig(this.iexec);
+    const requestOrderToSign = await orderModule.createRequestorder({
+      app: VALIDATOR_APP_ADDRESS,
+      appmaxprice: firstAppOrder?.order.appprice,
+      workerpoolmaxprice: workerPoolOrder?.order.workerpoolprice,
+      requester: userAddress,
+      volume: 1,
+      params: APP_CALL_PARAMETERS,
+      category: APP_CATEGORY
+    });
+    console.log('created requestOrder: ', requestOrderToSign);
+
+    // sign request order using initialized iexec
+    const requestOrder = await orderModule.signRequestorder(requestOrderToSign);
+    console.log('signed requestOrder:', requestOrder);
+
+    // matching orders
+    const res = await orderModule.matchOrders({
+      apporder: firstAppOrder?.order,
+      requestorder: requestOrder,
+      workerpoolorder: workerPoolOrder?.order
+    });
+    console.log(res);
+
   }
 }
 
