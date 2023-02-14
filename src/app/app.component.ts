3757ea57d36a20be5f037ce2bc628f9953665b55
luaroncrew <ma-kir.ru@yandex.ru>
2023-02-14 10:13:34 +0100

    import iexec, init metamask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IExec} from "iexec";
 
+const VALIDATOR_APP_ADDRESS = '0xFF1204Ce914006C7479b0E362A4AC6917F84D8b3';
+const APP_CALL_PARAMETERS = 'setup'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'IexecFrontend';
+
+export class AppComponent{
+  title: string = 'hellp';
+
+  initWallet() {
+    if (typeof window.ethereum !== 'undefined') {
+      console.log('MetaMask is installed!');
+    }
+  }
 }
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  allowSyntheticDefaultImports: true
 };
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
 
 /*
  * For easier debugging in development mode, you can import the following file
